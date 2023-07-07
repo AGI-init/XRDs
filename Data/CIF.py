9c984ca655f409f03c85a1463d3f0fd026ce3fb8
sam <slerman@u.rochester.edu>
2023-07-07 17:37:18 -0400

    Undid

--- Data/CIF.py
+++ Data/CIF.py
@@ -809,7 +809,7 @@ def generate(in_dir=None):
 
     name = in_dir.rstrip('/').split('/')[-1].replace('CIFs_', '')
 
-    num_workers = min(os.cpu_count(), 24)
+    num_workers = min(os.cpu_count(), 70)
 
     files = glob.glob(in_dir.rstrip('/') + '/*.cif')
     save_path = f'{root}Generated/XRDs_{name}/'
